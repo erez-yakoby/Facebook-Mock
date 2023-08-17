4b878ba11001a5219bd0438a852bbd22f40f10f0
Erez <yakobierez123@gmail.com>
2023-08-17 09:47:19 +0300

    switch to server side for now

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,14 +1,23 @@
 import { Box } from "@mui/material";
 import React from "react";
 import Post from "./Post";
+import Posts from "../data/Posts";
 
 const Feed = () => {
   return (
     <Box p={2} flex={4}>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {Posts.map((p) => {
+        return (
+          <Post
+            key={p.id}
+            userName={p.userName}
+            profileImgUrl={p.profileImgUrl}
+            content={p.content}
+            mainImgUrl={p.mainImgUrl}
+            date={p.date}
+          />
+        );
+      })}
     </Box>
   );
 };
