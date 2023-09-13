96fc133943ec04ef3361f419af4536c33073d66b
Erez Yakoby <yakobierez123@gmail.com>
2023-09-13 16:23:13 +0300

    created User schema and auth methods (#4)

--- server/index.js
+++ server/index.js
@@ -1,21 +1,33 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const helmet = require("helmet");
+// const helmet = require("helmet");
 const morgan = require("morgan");
 const app = express();
 const PORT = 3080;
+
+app.use(cors());
+
+// routes
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
 
 app.use(express.json());
-app.use(helmet());
+// app.use(helmet());
 app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+// app.use(logger);
+
+app.get("/", (req, res) => {
+  console.log("arrived");
+  res.send({ first: "posts" });
+});
 
 app.listen(PORT, (error) => {
   if (!error)
@@ -24,3 +36,7 @@ app.listen(PORT, (error) => {
     );
   else console.log("Error occurred, server can't start", error);
 });
+
+// things missing:
+// hide password when sending it to mongo
+//
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,12 +2,33 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 //register new user
-router.get("/register", async (req, res) => {
-  const user = await new User({
-    username: "erez",
+router.post("/register", async (req, res) => {
+  const newUser = await new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    profileImg: req.body.profileImg,
   });
-  await user.save();
-  res.send("register adress");
+  try {
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("error");
+    res.send("error");
+  }
+});
+
+//login user
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    (!user || req.body.password !== user.password) &&
+      res.status(400).json("Problem with email or password");
+  } catch (error) {
+    console.log("error");
+    res.send("error");
+  }
+  res.status(200).json(user);
 });
 
 module.exports = router;
