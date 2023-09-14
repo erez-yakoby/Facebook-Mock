521c52e8d721e32d0df5c8daf76bf2ffe52a8388
Erez Yakoby <yakobierez123@gmail.com>
2023-09-14 10:21:51 +0300

    Bcrypt passwords (#6)
    * hashed passwords in auth route
    
    * finished users route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,19 +1,29 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 //register new user
 router.post("/register", async (req, res) => {
-  const newUser = await new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    profileImg: req.body.profileImg,
-  });
   try {
+    // hashing the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // creating and saving the new user
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+      profileImg: req.body.profileImg,
+    });
+
+    // save user and return matching response
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (error) {
-    console.log("error");
+    // TODO: change the returning value
+
+    console.log(error);
     res.send("error");
   }
 });
@@ -21,14 +31,23 @@ router.post("/register", async (req, res) => {
 //login user
 router.post("/login", async (req, res) => {
   try {
+    // check that user exist by email
     const user = await User.findOne({ email: req.body.email });
-    (!user || req.body.password !== user.password) &&
-      res.status(400).json("Problem with email or password");
+    !user && res.status(400).json("Problem with email or password");
+
+    // check that that the given password matches the encrypted one
+    const isValidPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !isValidPassword
+      ? res.status(400).json("Problem with email or password")
+      : res.status(200).json(user);
   } catch (error) {
+    // TODO: change the returning value
     console.log("error");
     res.send("error");
   }
-  res.status(200).json(user);
 });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,16 +1,123 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// get user
+// delete user
+// follow user
+// unfollow user
+// update user
+
+// update user
+router.put("/:id", async (req, res) => {
+  if (req.body.senderId === req.params.id) {
+    if (req.body.password) {
+      // hash the password we want to change
+      try {
+        const salt = await bcrypt.salt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        console.log("error");
+        res.send("error");
+      }
+    }
+    try {
+      // update user
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      // responding
+      res.status(200).json("updated account");
+    } catch (error) {
+      console.log("error");
+      res.send("error");
+    }
+  } else {
+    res.status(403).json("You can update only your account");
+  }
+});
+
+// delete user
+router.delete("/:id", async (req, res) => {
+  // TODO: handle the case where the user does not exists
+  if (req.body.senderId === req.params.id) {
+    try {
+      // delete user
+      const user = await User.findByIdAndDelete(req.params.id);
+      // responding
+      res.status(200).json("deleted account");
+    } catch (error) {
+      console.log(error);
+      res.send("error");
+    }
+  } else {
+    res.status(403).json("You can delete only your account");
+  }
+});
 
 // get a user
-// TODO:  i am returning all of the user detaild including password!
 router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     !user && res.status(404).json("user not found");
-    res.status(200).json(user);
+
+    // removing unneccecery attributes
+    const minimalUser = { ...user._doc };
+    delete minimalUser.password;
+
+    // responding
+    res.status(200).json(minimalUser);
   } catch (error) {
     console.log("error");
     res.send("error");
+  }
+});
+
+// follow a user
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.senderId !== req.params.id) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id);
+      const senderUser = await User.findByIdAndUpdate(req.body.senderId);
+
+      // add to followers and following
+      if (!user.followers.includes(req.body.senderId)) {
+        await user.updateOne({ $push: { followers: req.body.senderId } });
+        await senderUser.updateOne({ $push: { following: req.params.id } });
+        res.status(200).json("successfully followed the user");
+      } else {
+        res.status(403).json("You already follow this user");
+      }
+    } catch (error) {
+      console.log("error");
+      res.send("error");
+    }
+  } else {
+    res.status(403).json("You can't follow yourself");
+  }
+});
+
+// unfollow a user
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.senderId !== req.params.id) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id);
+      const senderUser = await User.findByIdAndUpdate(req.body.senderId);
+
+      // remove from followers and following
+      if (user.followers.includes(req.body.senderId)) {
+        await user.updateOne({ $pull: { followers: req.body.senderId } });
+        await senderUser.updateOne({ $pull: { following: req.params.id } });
+        res.status(200).json("successfully unfollowed the user");
+      } else {
+        res.status(403).json("You dont follow this user");
+      }
+    } catch (error) {
+      console.log("error");
+      res.send("error");
+    }
+  } else {
+    res.status(403).json("You can't unfollow yourself");
   }
 });
 
